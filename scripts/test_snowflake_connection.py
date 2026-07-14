cfe7cb2b45d86ef17c78b4b4ba567a490879e235
cmsubhadra <74703174+cmsubhadra@users.noreply.github.com>
2026-07-14 19:16:05 +0530

    Tested snowflake connection using this file

---
+++ scripts/test_snowflake_connection.py
@@ -0,0 +1,28 @@
+import snowflake.connector
+
+try:
+    conn = snowflake.connector.connect(
+        user="",
+        password="",      
+        account="",
+        warehouse="ATMOSYNC_WH",
+        database="ATMOSYNC_DB",
+        schema="RAW"
+    )
+
+    print("✅ Connected to Snowflake Successfully!")
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT CURRENT_VERSION();")
+
+    version = cursor.fetchone()
+
+    print("Snowflake Version:", version[0])
+
+    cursor.close()
+    conn.close()
+
+except Exception as e:
+    print("❌ Connection Failed")
+    print(e)

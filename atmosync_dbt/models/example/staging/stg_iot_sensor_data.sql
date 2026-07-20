00706c4ff8c330b43a184caa28d9274b2b473b14
Nandhana.K.S <nandanks.jan13@gmail.com>
2026-07-20 10:22:10 +0530

    Implemented fact models

--- atmosync_dbt/models/example/staging/stg_iot_sensor_data.sql
+++ atmosync_dbt/models/example/staging/stg_iot_sensor_data.sql
@@ -1,3 +1,39 @@
-SELECT
-    *
-FROM ATMOSYNC_DB.RAW.IOT_SENSOR_DATA
+{{ config(materialized='view') }}
+
+WITH source_data AS (
+
+    SELECT *
+    FROM ATMOSYNC_DB.RAW.IOT_SENSOR_DATA WHERE 
+    COMMODITY IS NOT NULL AND DESTINATION IS NOT NULL AND TEMPERATURE_C IS NOT NULL
+    AND HUMIDITY_PERCENT IS NOT NULL AND VIBRATION_G IS NOT NULL
+
+),
+
+cleaned_data AS (
+
+    SELECT DISTINCT
+
+        CAST("TIMESTAMP" AS TIMESTAMP_NTZ) AS timestamp,
+
+        TRIM("CONTAINER_ID") AS container_id,
+        TRIM("COMMODITY") AS commodity,
+        TRIM("DESTINATION") AS destination,
+
+        CAST("TEMPERATURE_C" AS FLOAT) AS temperature_c,
+        CAST("HUMIDITY_PERCENT" AS FLOAT) AS humidity_percent,
+        CAST("VIBRATION_G" AS FLOAT) AS vibration_g,
+
+        CAST("LATITUDE" AS FLOAT) AS latitude,
+        CAST("LONGITUDE" AS FLOAT) AS longitude,
+
+        CAST("MARKET_PRICE_USD_PER_TON" AS NUMBER(10,2)) AS market_price_usd_per_ton,
+
+        TRIM("SPOILAGE_RISK") AS spoilage_risk,
+        TRIM("RECOMMENDED_ACTION") AS recommended_action
+
+    FROM source_data
+
+)
+
+SELECT *
+FROM cleaned_data
---
+++ atmosync_dbt/models/marts/fact_container_health.sql
@@ -0,0 +1,71 @@
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+
+    SELECT *
+    FROM {{ ref('stg_iot_sensor_data') }}
+
+)
+
+SELECT
+
+    timestamp,
+    container_id,
+    commodity,
+    destination,
+
+    temperature_c,
+    humidity_percent,
+    vibration_g,
+
+    latitude,
+    longitude,
+
+    market_price_usd_per_ton,
+
+    spoilage_risk,
+    recommended_action,
+
+    
+    -- Derived Business Columns
+
+
+    CASE
+        WHEN temperature_c < 5 THEN 'Low'
+        WHEN temperature_c <= 10 THEN 'Safe'
+        ELSE 'High'
+    END AS temperature_status,
+
+    CASE
+        WHEN humidity_percent < 60 THEN 'Low'
+        WHEN humidity_percent <= 80 THEN 'Normal'
+        ELSE 'High'
+    END AS humidity_status,
+
+    CASE
+        WHEN vibration_g < 2 THEN 'Normal'
+        ELSE 'High'
+    END AS vibration_status,
+
+    CASE
+        WHEN market_price_usd_per_ton < 1000 THEN 'Low Value'
+        WHEN market_price_usd_per_ton <= 2000 THEN 'Medium Value'
+        ELSE 'High Value'
+    END AS shipment_value_category,
+
+    CASE
+        WHEN spoilage_risk='High'
+             AND market_price_usd_per_ton>2000
+            THEN 'Critical'
+
+        WHEN spoilage_risk='High'
+            THEN 'High'
+
+        WHEN spoilage_risk='Medium'
+            THEN 'Medium'
+
+        ELSE 'Low'
+
+    END AS priority_level
+
+FROM source_data
